d5b83aef054b0b01ab62c432e426e2592b93311d
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-02-06 15:23:55 -0600

    file_io.py complete

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+foo_file = open('./src/foo.txt')
+print(foo_file.read())
+foo_file.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,5 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar_file = open('./src/bar.txt', 'w')
+bar_file.write("This is a new test of the century. Wow I wrote 'boogers' in a file, that I never even created how wacky is that. Swag. Yeet.")

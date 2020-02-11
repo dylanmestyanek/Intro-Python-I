1ee76cf0fec38af42b70447b650cea016e52593b
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-02-11 15:04:51 -0600

    Updated 08_comprehensions.py with proper list comprehension solution

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -36,6 +36,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = [filter(lambda v: float(v) % 2 == 0, x)]
+y = [int(num) for num in x if int(num) % 2 == 0]
 
 print(y)
